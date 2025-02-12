230ee23db7b2dac56645691731a941c2f284f7f6
Miguel Rodríguez <miguel@binpar.com>
2025-02-12 17:15:50 +0100

    chore: small refactor

--- src/utils/redrawHighlights.ts
+++ src/utils/redrawHighlights.ts
@@ -4,25 +4,28 @@ import { getState } from '@/utils/state';
 const redrawHighlights = () => {
   const state = getState();
 
-  console.log('redrawHighlights', state.userHighlightsById);
+  state.highlights.remove();
+  state.highlights.innerHTML = '';
+  if (state.layout === 'fixed') {
+    state.content.appendChild(state.highlights);
+  }
+  if (state.layout === 'flow') {
+    state.wrapper.appendChild(state.highlights);
+  }
 
-  if (state.userHighlightsById) {
-    state.highlights.remove();
-    state.highlights.innerHTML = '';
-    if (state.layout === 'fixed') {
-      state.content.appendChild(state.highlights);
-    }
-    if (state.layout === 'flow') {
-      state.wrapper.appendChild(state.highlights);
-    }
+  if (state.userHighlightsById?.size) {
+    const highlights = Array.from(state.userHighlightsById.values());
 
-    state.userHighlightsById.forEach((highlight) => {
-      const highlights = renderUserHighlight(highlight);
+    for (let i = 0, l = highlights.length; i < l; i++) {
+      const highlight = highlights[i];
+      if (highlight) {
+        const domHighlights = renderUserHighlight(highlight);
 
-      if (highlights) {
-        state.domHighlightsById.set(highlight.id, highlights);
+        if (domHighlights) {
+          state.domHighlightsById.set(highlight.id, domHighlights);
+        }
       }
-    });
+    }
   }
 };
 
